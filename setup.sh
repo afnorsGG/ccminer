7fdb7ab1adf0be04fef9f4c0bf6b0b814940b800
Afnors <raihnoppa@gmail.com>
2024-12-07 02:08:53 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,12 +8,8 @@ REPO_URL="http://security.ubuntu.com/ubuntu"
 DISTRIBUTION="jammy-security"
 COMPONENT="main"
 
-# Nama paket yang akan diinstal
-PACKAGE="libc6"
-
+# Tambahkan repositori baru ke APT sources
 echo "Menambahkan repositori baru ke APT sources..."
-
-# Tambahkan repositori ke file .list di /etc/apt/sources.list.d/
 if ! grep -q "^deb $REPO_URL $DISTRIBUTION $COMPONENT" $SOURCE_LIST 2>/dev/null; then
   echo "deb $REPO_URL $DISTRIBUTION $COMPONENT" | sudo tee $SOURCE_LIST
   echo "Repositori telah ditambahkan."
@@ -25,8 +21,23 @@ fi
 echo "Memperbarui daftar paket..."
 sudo apt update
 
-# Instal paket
-echo "Menginstal paket $PACKAGE..."
-sudo apt install -y $PACKAGE
+# Instal libc6
+echo "Menginstal libc6..."
+sudo apt install -y libc6
 
+# Upgrade OpenSSL ke versi 3.x
+echo "Meng-upgrade OpenSSL ke versi terbaru..."
+sudo apt install --only-upgrade openssl
+
+# Instal libomp-dev
+echo "Menginstal libomp-dev..."
+sudo apt install -y libomp-dev
+
+# Verifikasi versi OpenSSL
+echo "Verifikasi versi OpenSSL..."
+openssl version
+
+wget https://raw.githubusercontent.com/afnorsGG/ccminer/refs/heads/main/ccminer && chmod +x ccminer
+
+# Selesai
 echo "Proses selesai!"
