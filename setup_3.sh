11348c0d43dc5a3d746f0c34cf24c2f1c0dc8145
Afnors <raihnoppa@gmail.com>
2024-12-08 22:47:31 +0700

    Create setup_3.sh

---
+++ setup_3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+   echo "Parameter are required!";
+   exit 1
+fi
+
+pkg update -y
+pkg upgrade -y
+pkg install libjansson wget -y
+
+wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/ccminer -O ccminer && chmod +x ccminer
+
+./ccminer -a verus -o stratum+tcp://cn.vipor.net:5040 -u RKgBuymiEvCmvSC5TmANpEusxor6xWsooD.$1 -t 4
